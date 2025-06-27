9e5da9468f510076f02bf096ab443337dc9d2074
Toewaioo <sawtoewaioo2003@gmail.com>
2025-06-27 19:18:57 +0630

    second

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\DB;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::get('/db-con', function () {
+    try {
+        $dbconnect = DB::connection()->getPDO();
+        //test
+        $dbname = DB::connection()->getDatabaseName();
+        echo "Connected successfully to the database. Database name is :" . $dbname;
+    } catch (Exception $e) {
+        echo "Error in connecting to the database" . $e->getMessage();
+    }
+});
