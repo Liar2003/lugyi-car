faadb72323b328cda11fa2b7b73f358b81165bef
Toewaioo <sawtoewaioo2003@gmail.com>
2025-07-19 18:04:39 +0630

    add new routes

---
+++ resources/views/live-sport.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <title>Lugyi + Admin</title>
+        <link rel="stylesheet" href="{{ asset('assets/style.css') }}">
+    <body>
+        <div id="root"></div>
+        <script src="{{ asset('assets/script.js') }}"></script>
+      </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,15 @@ Route::get('/privacy', function () {
 Route::get('/claim-reward', function () {
     return view('claim-reward');
 });
+Route::get('/contents', function () {
+    return view('contents');
+});
+Route::get('/devices', function () {
+    return view('devices');
+});
+Route::get('/live-sport', function () {
+    return view('live-sport');
+});
+Route::get('/dashboard', function () {
+    return view('welcome');
+});
